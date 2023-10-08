849b093df8fafba2f884246f1fc6a9fcb3c7c76d
andres.aguirre <andresaguirreluna@gmail.com>
2023-10-08 09:48:21 -0500

    ruta imagen

--- Views/templates/header.php
+++ Views/templates/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Biblioteca</title>
-    <link rel="stylesheet" href="./Assets/css/estilos.css">
+    <link rel="stylesheet" href="/Assets/css/estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
@@ -20,7 +20,7 @@
     <nav class="navbar navbar-expand-lg navbar navbar-light" style="background-color: cadetblue;">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          <img src="../Assets/images/logo.png" alt="Logo" width="80%" height="80%" class="d-inline-block align-text-top">
+          <img src="./Assets/images/logo.png" alt="Logo" width="80%" height="80%" class="d-inline-block align-text-top">
           </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
